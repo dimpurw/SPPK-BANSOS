c35025d7c66ae3e4714c848eb521eaf680561b20
dimpurw <terminatorid2@gmail.com>
2021-10-14 23:20:15 +0700

    db

---
+++ database/migrations/2021_10_14_153838_create_kriteria_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKriteriaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kriteria', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode')->unique();
+            $table->string('nama');
+            $table->enum('atribut',['cost','benefit']);
+            $table->float('bobot', 8, 2);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kriteria');
+    }
+}
