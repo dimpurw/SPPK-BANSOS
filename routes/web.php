d6a979905af306d712d79dc5ceab33238828611c
dimpurw <terminatorid2@gmail.com>
2021-10-14 23:20:51 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/kriteria')->group(function ()
+{
+    Route::get('/', 'KriteriaController@index')->name('kriteria');
+    Route::get('/tambah', 'KriteriaController@create')->name('kriteria.tambah');
+    Route::post('/tambah', 'KriteriaController@store')->name('kriteria.simpan');
+    Route::get('/edit/{id}', 'KriteriaController@edit')->name('kriteria.edit');
+    Route::post('/edit/{id}', 'KriteriaController@update')->name('kriteria.update');
+    Route::post('/hapus/{id}', 'KriteriaController@destroy')->name('kriteria.hapus');
+});
