e73775c53ee59eb84a078b1b3c08a58106c48f1f
dimpurw <terminatorid2@gmail.com>
2021-10-18 00:24:15 +0700

    db

--- database/migrations/2018_05_27_174332_create_kriteria_table.php
+++ database/migrations/2018_05_27_174332_create_kriteria_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateKriteriaTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('kriteria', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('kode')->unique();
+            $table->string('nama');
+            $table->enum('atribut',['cost','benefit']);
+            $table->string('bobot');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('kriteria');
     }
 }
