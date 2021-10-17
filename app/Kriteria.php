187e22b8a0b1f7ba773279ca9ed3149d9fe696dc
dimpurw <terminatorid2@gmail.com>
2021-10-18 00:22:20 +0700

    model

--- app/Kriteria.php
+++ app/Kriteria.php
@@ -7,10 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Kriteria extends Model
 {
     protected $table        = 'kriteria';
-    protected $guarded      = [];
-
+    protected $fillable     = ['kode','nama','atribut','bobot'];
+    protected $hidden       = ['created_at','updated_at'];
     public function crip() {
         return $this->hasMany(\App\Crip::class);
     }
-
 }
